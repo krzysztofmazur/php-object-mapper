e36d83ba313ab3cbd55604c81ad64029e0de4a08
Krzysztof Mazur <krz@ychu.pl>
2017-02-17 01:10:49 +0100

    More unit tests

--- tests/KrzysztofMazur/ObjectMapper/Util/ReflectionTest.php
+++ tests/KrzysztofMazur/ObjectMapper/Util/ReflectionTest.php
@@ -2,9 +2,58 @@
 
 namespace Tests\KrzysztofMazur\ObjectMapper\Util;
 
+use KrzysztofMazur\ObjectMapper\Util\Reflection;
 use PHPUnit\Framework\TestCase;
+use TestFixtures\SimpleObject;
 
 class ReflectionTest extends TestCase
 {
+    public function testGetReflectionClassSuccess()
+    {
+        $reflectionClass = Reflection::getReflectionClass(SimpleObject::class);
 
+        self::assertInstanceOf(\ReflectionClass::class, $reflectionClass);
+        self::assertEquals(SimpleObject::class, $reflectionClass->getName());
+    }
+
+    /**
+     * @expectedException \ReflectionException
+     */
+    public function testGetReflectionClassFail()
+    {
+        Reflection::getReflectionClass("FakeClassNameXYZ");
+    }
+
+    public function testGetProperty()
+    {
+        $property = Reflection::getProperty(SimpleObject::class, "property1");
+
+        self::assertInstanceOf(\ReflectionProperty::class, $property);
+        self::assertEquals("property1", $property->getName());
+    }
+
+    public function testGetMethod()
+    {
+        $method = Reflection::getMethod(SimpleObject::class, "getProperty1");
+
+        self::assertInstanceOf(\ReflectionMethod::class, $method);
+        self::assertEquals("getProperty1", $method->getName());
+    }
+
+    public function testGetPropertyNames()
+    {
+        $properties = Reflection::getPropertyNames(SimpleObject::class);
+
+        self::assertCount(1, $properties);
+        self::assertContains('property1', $properties);
+    }
+
+    public function testGetMethodNames()
+    {
+        $methods = Reflection::getMethodNames(SimpleObject::class);
+
+        self::assertCount(2, $methods);
+        self::assertContains('getProperty1', $methods);
+        self::assertContains('setProperty1', $methods);
+    }
 }
